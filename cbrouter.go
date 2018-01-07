df994b3b5c1f05e341b1d65cfabf1f4a3ec8ef83
ardeshir sepahsalar <ardeshir.org@gmail.com>
2018-01-07 04:51:17 +0000

    adding api, client, main, resource

--- cbrouter.go
+++ cbrouter.go
@@ -1,10 +1,40 @@
 package rap
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
-type RouterFunc func(client *http.Client, content interface{} )
+// RouterFunc is the callback function type
+type RouterFunc func(resp *http.Response) error
 
+// CBRouter represents a collection of routers based on status codes
 type CBRouter struct {
-     Routers map[int]RouterFunc
-     DefaultRouter RouterFunc
+	Routers       map[int]RouterFunc
+	DefaultRouter RouterFunc
+}
+
+// NewRouter returns a new router
+func NewRouter() *CBRouter {
+	return &CBRouter{
+		Routers: make(map[int]RouterFunc),
+		DefaultRouter: func(resp *http.Response) error {
+			return fmt.Errorf("From: %s received unknown status: %d",
+				resp.Request.URL.String(), resp.StatusCode)
+		},
+	}
+}
+
+// RegisterFunc will register a function with a status code
+func (r *CBRouter) RegisterFunc(status int, fn RouterFunc) {
+	r.Routers[status] = fn
+}
+
+// CallFunc calls a registered function in the router
+func (r *CBRouter) CallFunc(resp *http.Response) error {
+	fn, ok := r.Routers[resp.StatusCode]
+	if !ok {
+		fn = r.DefaultRouter
+	}
+	return fn(resp)
 }
--- client.go
+++ client.go
@@ -1,43 +1,55 @@
 package rap
 
 import (
-    "net/http"
-    "strings"
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"strings"
 )
 
 type Client struct {
-     Client   *http.Client
-     AuthInfo Authentication
+	Client   *http.Client
+	AuthInfo Authentication
 }
 
 func NewClient() *Client {
-    return &Client {
-        Client: http.DefaultClient,
-    }
+	return &Client{
+		Client: http.DefaultClient,
+	}
 }
 
 func (c *Client) SetAuth(auth Authentication) {
-    c.AuthInfo = auth
+	c.AuthInfo = auth
 }
 
-func (c *Client) ProcessRequest(baseURL string, res *RestResource, params map[string]string ) {
-    
-     endpoint := strings.TrimLeft(res.RenderEndpoint(params), "/")    
-     trimmedBaseURL := strings.TrimRight(baseURL, "/")
-     url := trimmedBaseURL + "/" + endpoint
-     req, err := http.NewRequest(res.Method, url, nil)
-     if err != nil {
-         return err 
-     }
-     if c.AuthInfo != nil {
-         req.Header.Add("Authorization", c.AuthInfo.AuthorizationHeader())
-     }
-     resp, err := c.Client.Do(req)
-     if err != nil {
-         return err
-     }
-     if err := res.Router.CallFunc(resp); err != nil {
-         return err
-     }
-     return nil 
+func (c *Client) ProcessRequest(baseURL string, res *RestResource, params map[string]string, payload interface{}) error {
+	endpoint := strings.TrimLeft(res.RenderEndpoint(params), "/")
+	trimmedBaseURL := strings.TrimRight(baseURL, "/")
+	url := trimmedBaseURL + "/" + endpoint
+	req := buildClientRequest(res.Method, url, payload)
+	if c.AuthInfo != nil {
+		req.Header.Add("Authorization", c.AuthInfo.AuthorizationHeader())
+	}
+	resp, err := c.Client.Do(req)
+	if err != nil {
+		return err
+	}
+	return res.Router.CallFunc(resp)
+}
+
+func buildClientRequest(method, url string, payload interface{}) *http.Request {
+	if payload != nil {
+		payloadBytes, err := json.Marshal(payload)
+		if err != nil {
+			return nil
+		}
+		payloadBuffer := bytes.NewBuffer(payloadBytes)
+		req, err := http.NewRequest(method, url, payloadBuffer)
+		return req
+	}
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return nil
+	}
+	return req
 }
